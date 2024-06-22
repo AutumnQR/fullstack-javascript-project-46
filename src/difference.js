838f1590b4369bc1a0c9465904b529272c3aa1d0
AutumnQR <esthetic000@gmail.com>
2024-06-22 20:29:29 +0300

    codeclimate test

--- src/difference.js
+++ src/difference.js
@@ -1,57 +1,55 @@
 import _ from 'lodash';
 import { stylish, stringify } from './utils/stylish.js';
 
+const handleObjects = (key, fileOneValue, fileTwoValue, depth) => {
+  return stylish(
+    `  ${key}: ${compareFiles(fileOneValue, fileTwoValue, depth + 1)}`,
+    depth,
+  );
+};
+
+const handleMissingKey = (key, value, depth, exp) => {
+  return stylish(`${exp} ${key}: ${stringify(value, depth + 1)}`, depth);
+};
+
+const handleNotEqual = (key, fileOneValue, fileTwoValue, depth) => {
+  return [
+    stylish(`- ${key}: ${stringify(fileOneValue, depth + 1)}`, depth),
+    stylish(`+ ${key}: ${stringify(fileTwoValue, depth + 1)}`, depth),
+  ];
+};
+
+const handleEqualFiles = (key, fileOneValue, depth) => {
+  return stylish(`  ${key}: ${stringify(fileOneValue, depth + 1)}`, depth);
+};
+
+const compareFiles = (fileOne, fileTwo, depth = 1) => {
+  const allKeys = _.sortBy(_.union(Object.keys(fileOne), Object.keys(fileTwo)));
+
+  const diff = allKeys.map((key) => {
+    const [fileOneValue, fileTwoValue] = [fileOne[key], fileTwo[key]];
+    const isObjects = _.isObject(fileOneValue) && _.isObject(fileTwoValue);
+    const isFileHaveKey = (file) => _.has(file, key);
+
+    switch (true) {
+      case isObjects:
+        return handleObjects(key, fileOneValue, fileTwoValue, depth);
+      case !isFileHaveKey(fileTwo):
+        return handleMissingKey(key, fileOneValue, depth, '-');
+      case !isFileHaveKey(fileOne):
+        return handleMissingKey(key, fileTwoValue, depth, '+');
+      case fileOneValue !== fileTwoValue:
+        return handleNotEqual(key, fileOneValue, fileTwoValue, depth);
+      default:
+        return handleEqualFiles(key, fileOneValue, depth);
+    }
+  });
+
+  return stylish(diff, depth);
+};
+
 const genDiff = (data) => {
   const { fileOneData, fileTwoData } = data;
-
-  const compareFiles = (fileOne, fileTwo, depth = 1) => {
-    const fileOneKeys = Object.keys(fileOne);
-    const fileTwoKeys = Object.keys(fileTwo);
-
-    const allKeys = _.sortBy(_.union(fileOneKeys, fileTwoKeys));
-
-    const diff = allKeys.map((key) => {
-      const fileOneValue = fileOne[key];
-      const fileTwoValue = fileTwo[key];
-
-      // Возможно, этот участок кода не требуется, однако так код (switch/case) читается лучше
-      const isObjects = () =>
-        _.isObject(fileOneValue) && _.isObject(fileTwoValue);
-      const isFileHaveKey = (file) => _.has(file, key);
-
-      switch (true) {
-        case isObjects():
-          return stylish(
-            `  ${key}: ${compareFiles(fileOneValue, fileTwoValue, depth + 1)}`,
-            depth,
-          );
-        case !isFileHaveKey(fileTwo):
-          return stylish(
-            `- ${key}: ${stringify(fileOneValue, depth + 1)}`,
-            depth,
-          );
-
-        case !isFileHaveKey(fileOne):
-          return stylish(
-            `+ ${key}: ${stringify(fileTwoValue, depth + 1)}`,
-            depth,
-          );
-
-        case fileOneValue !== fileTwoValue:
-          return [
-            stylish(`- ${key}: ${stringify(fileOneValue, depth + 1)}`, depth),
-            stylish(`+ ${key}: ${stringify(fileTwoValue, depth + 1)}`, depth),
-          ];
-        default:
-          return stylish(
-            `  ${key}: ${stringify(fileOneValue, depth + 1)}`,
-            depth,
-          );
-      }
-    });
-
-    return stylish(diff, depth);
-  };
 
   return compareFiles(fileOneData, fileTwoData);
 };
