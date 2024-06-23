e20ee742e9706cd9bd8f8c75a9911e63558b9448
AutumnQR <esthetic000@gmail.com>
2024-06-23 13:46:23 +0300

    ...

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,13 +10,12 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format', 'stylish')
   .action((fileOne, fileTwo) => {
     const data = parseFile(fileOne, fileTwo);
     const options = program.opts();
-    const diff = genDiff(data, options.format || 'stylish');
+    const diff = genDiff(data, options.format);
     console.log(diff);
   });
-
-program.option('-f, --format [type]', 'output format', 'stylish');
 
 program.parse();
